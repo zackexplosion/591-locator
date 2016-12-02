7b269086c901e5b35e7cb702318b8c3e6a03d750
ZackExplosion <cstony0917@gmail.com>
2016-12-02 18:27:50 +0800

    add sample config file

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const CHANENEL = config.CHANENEL || "#test";
 const CHECK_FREQ = parseInt(config.CHECK_FREQ) * 1000 || 1000 * 60 * 1;
 const DEV = config.DEV || false;
 
-var API_ENDPOINT = 'http://rent.591.com.tw/index.php?module=search&action=rslist&is_new_list=1&type=1&searchtype=1&region=3&listview=img&rentprice=,20000&area=10,&floor=,1&order=posttime&orderType=desc';
-const WEB_HOOK_API = 'https://hooks.slack.com/services/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';
-const WEB_BASE_URI = 'http://www.591.com.tw/';
+var API_ENDPOINT = 'http://rent.591.com.tw/index.php?module=search&action=rslist&is_new_list=1&type=1&searchtype=1&region=3&listview=img&rentprice=,20000&area=10,&floor=,1&order=posttime&orderType=desc'
+const WEB_HOOK_API = config.WEB_HOOK_API
+const WEB_BASE_URI = 'http://www.591.com.tw/'
 
 
 const
@@ -107,42 +107,36 @@ const get_objects = function(main){
 
 var last_objects;
 
+let checker = function(){
+  parse(API_ENDPOINT, (result)=>{
+    // result = result.replace(/(\r\n|\n|\r)/gm,"");
+    result = JSON.parse(result);
 
-// the runner
-var runner = function(){
-  let checker = function(){
-    parse(API_ENDPOINT, (result)=>{
-      // result = result.replace(/(\r\n|\n|\r)/gm,"");
-      result = JSON.parse(result);
+    let object = result.main;
 
-      let object = result.main;
-
-      console.log('request done.');
-      let objects = get_objects(result.main);
+    console.log('request done.');
+    let objects = get_objects(result.main);
 
 
-      // if new object found send notification
-      if( typeof last_objects !== 'undefined'){
-        let new_objects = [];
-        for(var i in objects){
-          if( typeof last_objects[i] === 'undefined'){
-            new_objects.push(objects[i]);
-          }
-        }
-
-        if( new_objects.length > 0){
-          notification(new_objects);
+    // if new object found send notification
+    if( typeof last_objects !== 'undefined'){
+      let new_objects = [];
+      for(var i in objects){
+        if( typeof last_objects[i] === 'undefined'){
+          new_objects.push(objects[i]);
         }
       }
 
-      last_objects = objects;
-      runner();
-    });
-  }
-  setTimeout( checker, CHECK_FREQ);
+      if( new_objects.length > 0){
+        notification(new_objects);
+      }
+    }
+
+    last_objects = objects;
+    runner();
+  });
 }
 
-
-// start runner
-runner();
+checker();
+setInterval(checker, CHECK_FREQ)
 
